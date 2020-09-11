caf3f51f4f9f3091920f6389bede2bf192483c42
Lee-Juntong <2310266421@qq.com>
2020-09-11 22:30:55 +0800

    handle errors of empty todo and wrong command

--- src/main/java/duke/Main.java
+++ src/main/java/duke/Main.java
@@ -1,5 +1,10 @@
+package duke;
+
 import java.util.Scanner;
 
+import duke.exception.NonCommandException;
+import duke.exception.TodoException;
+import duke.exception.WrongCommandException;
 
 public class Main {
     public static final int MAX_TASK_CAPACITY = 100;
@@ -31,19 +36,31 @@ public class Main {
             }
             //the above are single word command
 
+
             numTask = respondMultiWordCommand(tasks, command, numTask);
+
+
         }
 
 
         printExitMessage();
     }
 
-    private static int respondMultiWordCommand(Task[] tasks, String command, int numTask) {
+    private static int respondMultiWordCommand(Task[] tasks, String command, int numTask)  {
         String[] words = command.split(" ");
         if (words[0].equals(TASK_DONE)) {
             tasks[Integer.parseInt(words[1]) - 1].markAsDone();
         } else {
-            numTask = addTask(words[0], tasks, command, numTask);
+
+            try {
+                numTask = addTask(words[0], tasks, command, numTask);
+            } catch (WrongCommandException e) {
+                printLine();
+                System.out.println("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                printLine();
+            } catch (TodoException e){
+                System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+            }
         }
         return numTask;
     }
@@ -60,14 +77,26 @@ public class Main {
 
     private static void printGreetingMessage() {
         printLine();
-        System.out.println("Hello! I'm Duke");
+
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
         System.out.println("What can I do for you?");
     }
 
-    private static int addTask(String beginning, Task[] tasks, String command, int numTask) {
-        printLine();
+    private static int addTask(String beginning, Task[] tasks, String command, int numTask) throws WrongCommandException, TodoException {
+
         int dividerPosition;
-        System.out.println("Got it. I've added this task: ");
+        if (!beginning.equals(ADD_TODO)
+                && !beginning.equals(ADD_DEADLINE)
+                && !beginning.equals(ADD_EVENT)) {
+            throw new WrongCommandException();
+        }
+
         switch (beginning) {
             case ADD_EVENT:
                 dividerPosition = command.indexOf("/at");
@@ -78,12 +107,19 @@ public class Main {
                 tasks[numTask] = new Deadline(command.substring(9, dividerPosition), command.substring(dividerPosition + 4));
                 break;
             case ADD_TODO:
-                tasks[numTask] = new Todo(command.substring(5));
+                try {
+                    tasks[numTask] = new Todo(command.substring(5));
+                } catch(StringIndexOutOfBoundsException e){
+                    throw new TodoException();
+                }
                 break;
             default:
-                System.out.println("芜湖 起飞");
                 break;
         }
+
+        //printing works below
+        printLine();
+        System.out.println("Got it. I've added this task: ");
         System.out.println(tasks[numTask].toString());
         //if you don't enter event/deadline/todo here, then there will be error as a null pointer is being accessed
         numTask++;
